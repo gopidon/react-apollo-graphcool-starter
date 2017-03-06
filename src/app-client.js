040b4a521aec0b00d0696874ee39ccff6878c490
udemydon <udemy.don@gmail.com>
2017-03-06 12:31:26 +0530

    2nd commit

--- src/app-client.js
+++ src/app-client.js
@@ -4,6 +4,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoutes from './components/AppRoutes';
+import "jquery";
+import "bootstrap";
+require('../node_modules/fixed-data-table/dist/fixed-data-table.min.css');
+require('./static/css/quill.base.css');
+require('./static/css/quill.snow.css');
+require('../node_modules/react-datepicker/dist/react-datepicker.min.css');
+require('./static/css/style.css');
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
 
 
 
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,16 +2,16 @@ import webpack from 'webpack';
 import path from 'path';
 
 export default {
-	  debug: true,
-	  noInfo: false,
-	  devtool: 'inline-source-map',
-    entry: ['babel-polyfill',path.join(__dirname, 'src', 'app-client.js')],
-	  target: 'web',
-    output: {
-        path: path.join(__dirname, 'src', 'static', 'js'),
-			  publicPath:"/js",
-        filename: 'bundle.js'
-    },
+	    debug: true,
+	    noInfo: false,
+	    devtool: 'inline-source-map',
+        entry: ['babel-polyfill',path.join(__dirname, 'src', 'app-client.js')],
+	    target: 'web',
+        output: {
+            path: path.join(__dirname, 'src', 'static', 'js'),
+            publicPath:"/js",
+            filename: 'bundle.js'
+	    },
     watch: true,
     module: {
         loaders: [{
@@ -24,12 +24,21 @@ export default {
                 }
             },
             //{ test: /\.css$/, loader: "style-loader!css-loader" },
-						{ test: /\.css$/, loaders: ['style','css'] },
-            { test: /\.json$/, loader: "json-loader"}]
+            { test: /\.css$/, loaders: ['style','css'] },
+            { test: /\.json$/, loader: "json-loader"},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+            {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+            ]
     },
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify("dev")
-        })
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
     ]
 };
