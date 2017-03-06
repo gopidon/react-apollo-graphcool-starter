b8967631b9e6ed66a974fe481293553044cae4ee
udemydon <udemy.don@gmail.com>
2017-03-06 12:47:41 +0530

    .

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -23,9 +23,8 @@ export default {
                     //presets: ['react','es2015', 'stage-0']
                 }
             },
-            //{ test: /\.css$/, loader: "style-loader!css-loader" },
-            { test: /\.css$/, loaders: ['style','css'] },
-            { test: /\.json$/, loader: "json-loader"},
+            {test: /\.css$/,loader: "style-loader!css-loader" },
+            {test: /\.json$/, loader: "json-loader"},
             {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
             {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
             {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
